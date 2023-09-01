0a49f91953308fbae77f5c40f7fe2de34db3b5c6
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2023-09-01 20:57:30 -0300

    initial comits

---
+++ FriendFace/FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Isaque da Silva on 01/09/23.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
